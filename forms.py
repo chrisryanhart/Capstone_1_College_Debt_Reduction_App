3675a3e0c391aea38a127198fdc2c7a2dcafaf86
rrghiker <chris.ryan.hart1@gmail.com>
2022-09-06 12:46:10 -0400

    Formatted app with bootstrap & CSS

--- forms.py
+++ forms.py
@@ -7,17 +7,6 @@ from enum import Enum
 # from seed import states
 from all_majors_seed import states, majors, unique_school_list, unique_major_titles
 
-# class Income(Enum):
-
-                        #         "75000-plus": 22909,
-    #  "0-48000": 12900,
-    #  "30001-75000": 15139,
-#     '0-10' = 1
-                        #    "0-30000": 12593,
-                        #    "30001-48000": 13424,
-                        #    "48001-75000": 16708,
-                        #    "75001-110000": 20959
-                        #    "110001-plus": 23946,
 
 incomes = [("0-30000","0-$30k"),("30001-48000","$30k-$48k"),("48001-75000","$48k-$75k"),("75001-110000","$75k-$110k"),("110001-plus","$110k+")]
 
@@ -32,17 +21,18 @@ class SearchForm(FlaskForm):
 
 
 class AddUserForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired(),Length(min=4,max=25)])
-    password = PasswordField('password', validators=[DataRequired(),Length(min=4,max=25)])
-    state = StringField('State of Residency:', validators=[DataRequired(),AnyOf(states,message='Invalid state format. Select state from dropdown list')])
-    household_income = SelectField('Household Income:', choices=incomes, validators=[DataRequired()])
+    username = StringField('Username: ', validators=[DataRequired(),Length(min=4,max=25)])
+    password = PasswordField('Password: ', validators=[DataRequired(),Length(min=4,max=25)])
+    state = StringField('State of Residency: ', validators=[DataRequired(),AnyOf(states,message='Invalid state format. Select state from dropdown list')])
+    household_income = SelectField('Household Income: ', choices=incomes, validators=[DataRequired()])
 
 class EditUserForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired(),Length(min=4,max=25)])
+    username = StringField('Username: ', validators=[DataRequired(),Length(min=4,max=25)])
     state = StringField('State of Residency:', validators=[DataRequired(),AnyOf(states,message='Invalid state format. Select state from dropdown list')])
     household_income = SelectField('Household Income:', choices=incomes, validators=[DataRequired()])
+    password = PasswordField('Confirm Password: ', validators=[DataRequired()])
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired(),Length(min=4,max=25)])
-    password = PasswordField('password', validators=[DataRequired(),Length(min=4,max=25)])
+    username = StringField('Username: ', validators=[DataRequired(),Length(min=4,max=25)])
+    password = PasswordField('Password: ', validators=[DataRequired(),Length(min=4,max=25)])
